c4236a61a2be44fc482c15f1bdd73a4219f2a452
tbrodbeck <tbrodbeck@uos.de>
2018-11-18 00:16:02 +0100

    fix and planing grid-manager

--- grid_search.py
+++ grid_search.py
@@ -9,9 +9,9 @@ from model import VRP
 
 ''' HYPERPARAMETERS'''
 
-run_name = 'test2'
+run = 'test2'
 
-test_heuristic = [
+heuristic_test = [
     'scenario', [1, 2],
     'heuristic', [False, True],
     'pop_size', [12],
@@ -28,21 +28,29 @@ test_heuristic = [
     'time_limit', [600]
 ]
 
-test_other = [
+test2 = [
     'scenario', [1, 2],
     'heuristic', [True],
     'pop_size', [10, 14],
     'selection_size', [2, 4],
     'aco_iterations', [15, 25],
+    'beta', [1],
+    'evap_rate', [.1],
+    'beta_evap', [0],
     'crossover_prob', [0.1, 0.2],
     'mutation_prob', [0.05, 0.1],
     'reduce_clusters', [0, 4],
-    'kmeans_iterations', [10, 20]
+    'kmeans_iterations', [10, 20],
+    'squared_dist', [True],
+    'time_limit', [600]
 ]
+
+input_dict = globals()[run]
+use_grid = True
 
 def create_dict(list_dict):
     """
-    Creating a dict from the list-dict
+    Creating a dict from a list-dict
     """
     # True if done with one key-value pair
     done = False
@@ -59,7 +67,7 @@ def create_dict(list_dict):
             continue
     return dict
 
-def gridsearch(dict_list):
+def gridsearch(dict_list, run_name, use_grid):
 
     dict = create_dict(dict_list)
 
@@ -79,37 +87,50 @@ def gridsearch(dict_list):
     # deploying all permutations
     print('All permutations of test-parameters:')
     nr_of_perms = 0
+    permutations = []
     for v in (itertools.product(*iteration)):
+        permutations.append(v)
         print(v)
         nr_of_perms += 1
     with open('./results/' + run_name + '.pickle', 'wb') as f:
         pickle.dump(dict_list, f)
 
 
-    count = 0
-    for v in (itertools.product(*iteration)):
-        count += 1
-        print('Test', count, 'of', nr_of_perms, ':')
-        print('scenario:', v[0], 'heuristic:', v[1], 'pop_size:', v[2], 'selection_size:', v[3], 'aco_iterations:', v[4], 'beta:', v[5], 'evap_rate:', v[6], 'beta_evap:', v[7], 'crossover_prob:', v[8], 'mutation_prob:', v[9], 'reduce_clusters:', v[10], 'kmeans_iterations:', v[11], 'squared_dist:', v[12], 'time_limit:', v[13])
-        best_run, mean_run = VRP(scenario=v[0], heuristic=v[1], pop_size=v[2], selection_size=v[3], aco_iterations=v[4], beta=v[5], evap_rate=v[6], beta_evap=v[7], crossover_prob=v[8], mutation_prob=v[9], reduce_clusters=v[10], kmeans_iterations=v[11], squared_dist=v[12], time_limit=v[13])
+    if use_grid == False:
+        count = 0
+        for v in permutations:
+            count += 1
+            print('Test', count, 'of', nr_of_perms, ':')
+            print(dict_list)
+            best_run, mean_run = VRP(scenario=v[0], heuristic=v[1], pop_size=v[2], selection_size=v[3], aco_iterations=v[4], beta=v[5], evap_rate=v[6], beta_evap=v[7], crossover_prob=v[8], mutation_prob=v[9], reduce_clusters=v[10], kmeans_iterations=v[11], squared_dist=v[12], time_limit=v[13])
 
-        # saving results
-        best.append(best_run)
-        mean.append(mean_run)
+            # saving results
+            best.append(best_run)
+            mean.append(mean_run)
+        # persisting
+        print('Endresults for best:')
+        with open('./results/' + run_name + '_best.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            for i in best:
+                print(i)
+                writer.writerow(i)
+        print('Endresults for mean:')
+        with open('./results/' + run_name + '_mean.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            for i in mean:
+                print(i)
+                writer.writerow(i)
 
-    # persisting
-    print('Endresults for best:')
-    with open('./results/' + run_name + '_best.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        for i in best:
-            print(i)
-            writer.writerow(i)
-    print('Endresults for mean:')
-    with open('./results/' + run_name + '_mean.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        for i in mean:
-            print(i)
-            writer.writerow(i)
+    else:
+        grid_manager(permutations)
+
+def grid_manager(permutations):
+    """
+    using a special manager for IKW grid deployment
+    :param permutations: all permutations of the input to deploy
+    """
+    pass
+
 
 if __name__ == '__main__':
-    gridsearch(test_heuristic)
+    gridsearch(input_dict, run, use_grid)
---
+++ search_manager.py
@@ -0,0 +1,15 @@
+from grid_search import gridsearch
+from plotting import plot
+
+test2 = [
+    'scenario', [1, 2],
+    'heuristic', [True],
+    'pop_size', [10, 14],
+    'selection_size', [2, 4],
+    'aco_iterations', [15, 25],
+    'crossover_prob', [0.1, 0.2],
+    'mutation_prob', [0.05, 0.1],
+    'reduce_clusters', [0, 4],
+    'kmeans_iterations', [10, 20]
+]
+
