df0b552304a4e3b8d3761a3de80f8c2f68a70e75
tbrodbeck <tbrodbeck@uos.de>
2018-11-18 08:09:58 +0100

    ikw_grid deploy v0

--- grid_search.py
+++ grid_search.py
@@ -28,7 +28,7 @@ further_test = [
     'reduce_clusters', [0, 4],
     'kmeans_iterations', [10, 20],
     'squared_dist', [True],
-    'time_limit', [20] # in minutes
+    'time_limit', [10] # in minutes
 ]
 
 heuristic_test = [
@@ -45,7 +45,7 @@ heuristic_test = [
     'reduce_clusters', [0],
     'kmeans_iterations', [10],
     'squared_dist', [True],
-    'time_limit', [20] # in minutes
+    'time_limit', [10] # in minutes
 ]
 
 
@@ -106,7 +106,7 @@ def gridsearch(dict_list, run_name, use_ikw_grid=False):
         count = 0
         for v in permutations:
             count += 1
-            print('Test', count, 'of', nr_of_perms, ':')
+            print(run_name, count, 'of', nr_of_perms, ':')
             print(dict_list)
             best_run, mean_run = VRP(scenario=v[0], heuristic=v[1], pop_size=v[2], selection_size=v[3], aco_iterations=v[4], beta=v[5], evap_rate=v[6], beta_evap=v[7], crossover_prob=v[8], mutation_prob=v[9], reduce_clusters=v[10], kmeans_iterations=v[11], squared_dist=v[12], time_limit=v[13] * 60)
 
---
+++ ikw_grid_deploy.py
@@ -0,0 +1,45 @@
+import sys
+
+from grid_search import gridsearch, create_dict
+
+
+# parsing command-line imputs
+parameter = sys.argv[1]
+value1 = int(sys.argv[2])
+value2 = int(sys.argv[3])
+
+
+dict_list = [
+    'scenario', [1, 2],
+    'heuristic', [True],
+    'pop_size', [12],
+    'selection_size', [2],
+    'aco_iterations', [20],
+    'beta', [1],
+    'evap_rate', [.1],
+    'beta_evap', [0],
+    'crossover_prob', [0.15],
+    'mutation_prob', [0.07],
+    'reduce_clusters', [4],
+    'kmeans_iterations', [15],
+    'squared_dist', [True],
+    'time_limit', [20] # in minutes
+]
+
+def search_init(dict_list, parameter, test_values):
+    """
+    searches
+    :param dict_list: standard parameters
+    :param parameter: param we want to analyze
+    :param test_values: how we want to change the parameter
+    """
+    for i, e in enumerate(dict_list):
+        if e == parameter:
+            dict_list[i + 1] = test_values
+    print(dict_list)
+
+    gridsearch(dict_list, parameter + str(test_values))
+
+
+search_init(dict_list, parameter, value1, value2)
+
