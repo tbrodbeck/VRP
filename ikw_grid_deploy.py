64bb1a6bb33871b5e7103a795e762cd9979585af
Inga Kramarek <ikramarek@uos.de>
2018-11-19 17:50:43 +0100

    optimized parameter values

--- ikw_grid_deploy.py
+++ ikw_grid_deploy.py
@@ -13,23 +13,23 @@ dict_list = [
     'scenario', [1, 2],
     'heuristic', [True],
     'pop_size', [14],
-    'selection_size', [4],
-    'aco_iterations', [15],
-    'beta', [1],
+    'selection_size', [6],
+    'aco_iterations', [17],
+    'beta', [0.99],
     'evap_rate', [.1],
-    'beta_evap', [0.2],
-    'crossover_prob', [0.1],
-    'mutation_prob', [0.1],
-    'reduce_clusters', [4],
+    'beta_evap', [0.1],
+    'crossover_prob', [0.7],
+    'mutation_prob', [0.07],
+    'reduce_clusters', [6],
     'kmeans_iterations', [20],
-    'squared_dist', [False],
+    'squared_dist', [True],
     'time_limit', [20] # in minutes
 ]
 
 def search_init(dict_list, parameter, test_values):
     """
     searches
-    :param dict_list: standa.rd parameters
+    :param dict_list: standard parameters
     :param parameter: param we want to analyze
     :param test_values: how we want to change the parameter
     """
--- scheduler_ikw.py
+++ scheduler_ikw.py
@@ -10,33 +10,36 @@ command = 'python3 ikw_grid_deploy.py '
 command2 = 'python3 plotting.py '
 
 if run == 1:
-	input = 'pop_size 12 16'
+	input = 'pop_size 14 16'
 
 if run == 2:
-	input = 'selection_size 3 6'
+	input = 'selection_size 6 8'
 
 if run == 3:
-	input = 'aco_iterations 10 20'
+	input = 'aco_iterations 15 20'
 
 if run == 4:
-	input = 'beta 0.99 1'
+	input = 'beta 0.95 0.99'
 
 if run == 5:
-	input = 'evap_rate .1 .3'
+	input = 'evap_rate .05 .15'
 
 if run == 6:
-	input = 'crossover_prob 0.05 0.15'
+	input = 'beta_evap 0 0.2'
 
 if run == 7:
-	input = 'mutation_prob 0.07 0.13'
+	input = 'crossover_prob 0.05 0.1'
 
 if run == 8:
-	input = 'reduce_clusters 2 6'
+	input = 'mutation_prob 0.05 0.1'
 
 if run == 9:
-	input = 'kmeans_iterations 20 30'
+	input = 'reduce_clusters 4 8'
 
 if run == 10:
+	input = 'kmeans_iterations 15 25'
+
+if run == 11:
 	input = 'squared_dist 0 1'
 
 os.system(command + input)
